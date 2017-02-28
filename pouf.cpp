19bbd609fa9f37110331bd8bdb3076069818095d
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-02-28 17:35:18 +0100

    cosmetics

--- pouf.cpp
+++ pouf.cpp
@@ -13,13 +13,13 @@
 
 std::string demangle(const char* name) {
 
-  int status = -4; // some arbitrary value to eliminate the compiler warning
+  int status;
 
   // enable c++11 by passing the flag -std=c++11 to g++
-  std::unique_ptr<char, void(*)(void*)> res {
+  std::unique_ptr<char, void(*)(void*)> res = {
     abi::__cxa_demangle(name, NULL, NULL, &status),
-      std::free
-      };
+    std::free
+  };
 
   return (status==0) ? res.get() : name ;
 }
