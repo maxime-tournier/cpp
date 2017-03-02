f1fc573807774fe7a831155132a7ebc7be2580a1
Maxime Tournier <tournier.maxime@gmail.com>
2017-03-02 10:15:57 +0100

    efficient concatenation

--- pouf.cpp
+++ pouf.cpp
@@ -753,7 +753,53 @@ static void with_auto_stack( const F& f ) {
 }
 
 
+static void concatenate(rmat& res, const rmat& src) {
+  res.resize(src.rows(), src.cols());
 
+  std::vector<bool> mask(src.cols());
+  std::vector<real> values(src.cols());
+  std::vector<int> indices(src.cols());  
+  
+  // TODO better estimate? 
+  const int estimated_nnz = src.nonZeros() + src.nonZeros() / 2;
+  res.reserve(estimated_nnz);
+  
+  std::fill(mask.begin(), mask.end(), false);
+  
+  for(unsigned i = 0, n = src.rows(); i < n; ++i) {
+	unsigned nnz = 0;
+	
+	for(rmat::InnerIterator src_it(src, i); src_it; ++src_it) {
+	  const real y = src_it.value();
+	  const unsigned k = src_it.col();
+
+	  // take row from previously computed if possible
+	  for(rmat::InnerIterator res_it(k < i ? res : src, k); res_it; ++res_it) {
+		const unsigned j = res_it.col();
+        const real x = res_it.value();
+
+		if(!mask[j]) {
+		  mask[j] = true;
+		  values[j] = x * y;
+		  indices[nnz] = j;
+          ++nnz;
+		} else {
+		  values[j] += x * y;
+		}
+	  }
+	}
+
+	res.startVec(i);
+	for(unsigned k = 0; k < nnz; ++k) {
+	  const unsigned j = indices[k];
+	  res.insertBack(i, j) = values[j];
+	  mask[j] = false;
+	}
+	
+  }
+  
+  res.finalize();
+}
 
 
 int main(int, char**) {
@@ -823,12 +869,11 @@ int main(int, char**) {
   std::cout << "before concatenation: " << std::endl;
   std::cout << jacobian << std::endl;
 
-  for(unsigned v : order) {
-    g[v].apply(concatenate(jacobian, chunks), v);
-  }
-
+  rmat concat;
+  concatenate(concat, jacobian);
+  
   std::cout << "after concatenation: " << std::endl;
-  std::cout << jacobian << std::endl;
+  std::cout << concat << std::endl;
   
   
   return 0;
