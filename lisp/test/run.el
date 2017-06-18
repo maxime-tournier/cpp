a435b551fb9ebfbf05559c9af95bcd61e6a5de69
max <max@localhost.localdomain>
2017-06-18 11:59:56 +0200

    okay

--- lisp/test/run.el
+++ lisp/test/run.el
@@ -1,11 +1,14 @@
 
+;; this is ok
 (run
  (var x (ref nil))
  (get x))
 
 
-;; (run (ref nil))
+;; this is not ok: inner thread escapes
+(run (ref nil))
 
-;; (var x (ref nil))
-;; (run (get x))
+;; this is not ok: inner thread references outer thread and maybe unpure
+(var x (ref nil))
+(run (get x))
 
