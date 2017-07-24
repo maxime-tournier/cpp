62325f22456573cbff4fe3636f28918c086979fa
max <max@localhost.localdomain>
2017-07-24 20:37:17 +0200

    record test

---
+++ slip/test/record.el
@@ -0,0 +1,11 @@
+(def x (record (foo 1) (marcel "derp") (bar 2)))
+
+(@marcel x)
+
+(def foo-bar (lambda (x p)
+               (cond (p (@foo x))
+                     (true (@bar x)))))
+
+(foo-bar x false)
+                        
+
