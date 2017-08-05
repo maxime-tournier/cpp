821221133a60d69e88d176278ec1b60da6fe126f
max <max@localhost.localdomain>
2017-08-05 20:33:08 +0200

    curry

--- slip/test/curry.el
+++ slip/test/curry.el
@@ -1,6 +1,6 @@
 
 
-;; extra args on functions returning functions
+;; over-saturated calls
 (def over1 (lambda (x) (lambda (y) (lambda (z) (+ z (+ x y))))))
 (def over2 (lambda (x) (lambda (y z) (+ z (+ x y)))))
 (def over3 (lambda (x y) (lambda (z) (+ z (+ x y)))))
@@ -9,7 +9,7 @@
 (over2 1 2 3)
 (over3 1 2 3)
 
-;; ;; curryfied functions
+;; partial applications
 ;; (def add1 (+ 1))
 ;; (add1 2)
 
