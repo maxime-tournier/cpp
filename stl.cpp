27f09a8541024ff1d7ce58b9ba4a1911773940e4
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-08-29 18:58:31 +0200

    stl is_binary

--- stl.cpp
+++ stl.cpp
@@ -92,6 +92,35 @@ namespace stl {
     return in;
   }
 
+
+  static bool is_binary(std::istream& in) {
+    
+    in.seekg(0, std::ios::end);
+    const std::size_t file_size = in.tellg();
+
+    in.seekg(0, std::ios::beg);    
+    char header[80];
+
+    if(!in.read(header, sizeof(header)) ) {
+      return false;
+    }
+
+    uint32 triangles;
+    if(!in.read(triangles.bytes, sizeof(triangles.bytes))) {
+      return false;
+    }
+    
+    static const std::size_t float_size = 4;
+    static const std::size_t vec3_size = 3 * float_size;
+    static const std::size_t triangle_size = 4 * vec3_size + 2;
+    static const std::size_t header_size = 80 + 4;
+
+    const std::size_t expected_size = header_size + triangles.value * triangle_size;
+
+    return file_size == expected_size;
+  }
+  
+  
 }
 
 
