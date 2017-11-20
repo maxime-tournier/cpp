a6a489419ba6ab999363cb082be7e7324062b8a2
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-11-20 15:47:44 +0100

    hop

--- nan_union.cpp
+++ nan_union.cpp
@@ -1,4 +1,4 @@
-// -*- compile-command: "c++ -g -Wall -std=c++11 nan_union.cpp -o nan_union" -*-
+// -*- compile-command: "c++ -g -Wall -std=c++11 nan_union.cpp -o nan_union -lstdc++" -*-
 
 #include "nan_union.hpp"
 #include "ref.hpp"
@@ -25,62 +25,111 @@ struct test {
 };
 
 
-class derp {
+class nan_boxed {
 protected:
   ieee754 data;
-  using payload_type = ieee754::payload_type;
-
+  using value_type = std::int64_t;
+  
   void release() {
-    payload_type tmp = data.bits.payload;
-    reinterpret_cast<ref_any&>(tmp).~ref_any();
+    value_type tmp = data.bits.payload;
+    reinterpret_cast<ref_any&>(tmp) = {};
   }
-
+  
 public:
-  
-  template<class T>
-  void set(const T& value) {
-    static_assert( sizeof(T) <= sizeof(payload_type), "type too large");
-    if(data.bits.nan && data.bits.flag) release();
 
-    if(std::is_same<T, double>::value) {
-      data.value = reinterpret_cast<const double&>(value);
+  ref_any as_ref() const {
+    if(!data.bits.nan || !data.bits.flag) throw std::bad_cast();
+    value_type tmp = data.bits.payload;
+    return reinterpret_cast<ref_any&>(tmp);
+  }
+
+  const double& as_double() const {
+    if(data.bits.nan) throw std::bad_cast();
+    return data.value;
+  }
+
+  value_type as_value() const {
+    if(!data.bits.nan || data.bits.flag) throw std::bad_cast();
+    return data.bits.payload;
+  }
+  
+  nan_boxed& operator=(const value_type& other) {
+    if(data.bits.flag) release();
+    data.bits.flag = false;
+    data.bits.nan = ieee754::quiet_nan;
+    data.bits.payload = other;
+    return *this;
+  }
+
+  nan_boxed& operator=(const double& other) {
+    if(data.bits.flag) release();
+    data.value = other;
+    return *this;
+  }
+
+  nan_boxed& operator=(const ref_any& other) {
+    if(!data.bits.flag) {
+      data.bits.flag = true;
+      data.bits.nan = ieee754::quiet_nan;
+    }
+
+    value_type value;
+    new (&value) ref_any(other);
+    data.bits.payload = value;
+    
+    return *this;
+  }
+
+  nan_boxed& operator=(ref_any&& other) {
+    if(!data.bits.flag) {
+      data.bits.flag = true;
+      data.bits.nan = ieee754::quiet_nan;
+    }
+
+    value_type value;
+    new (&value) ref_any(std::move(other));
+    data.bits.payload = value;
+    
+    return *this;
+  }
+
+
+  ~nan_boxed() {
+    if(data.bits.flag) release();
+  }
+
+  nan_boxed(const nan_boxed& other) {
+    if(other.data.bits.flag) {
+      value_type value;
+
+      // TODO optimize temporary
+      new (&value) ref_any(other.as_ref());
+      data.bits.payload = value;
+      data.bits.flag = true;
+      data.bits.nan = ieee754::quiet_nan;
     } else {
-      data.bits.flag = false;
-      data.bits.nan = ieee754::quiet_nan;
-      data.bits.payload = reinterpret_cast<const ieee754::payload_type&>(value);
+      data.value = other.data.value;
     }
   }
 
-  void set(const ref_any& value) {
-    if(!data.bits.nan || !data.bits.flag) {
-      // turn the data into a null ptr if it wasnt a ptr
-      data.bits.flag = true;
-      data.bits.nan = ieee754::quiet_nan;
-      data.bits.payload = 0;
-    }
 
-    payload_type tmp = data.bits.payload;
-    reinterpret_cast<ref_any&>(tmp) = value;
-    data.bits.payload = tmp;    
+  nan_boxed(const ref_any& other) {
+    value_type value;
+    new (&value) ref_any(other);
+    data.bits.flag = true;
+    data.bits.nan = ieee754::quiet_nan;
+    data.bits.payload = value;
   }
 
-  void set(ref_any&& value) {
-    if(!data.bits.nan || !data.bits.flag) {
-      // turn the data into a null ptr if it wasnt a ptr
-      data.bits.flag = true;
-      data.bits.nan = ieee754::quiet_nan;
-      data.bits.payload = 0;
-    }
-
-    payload_type tmp = data.bits.payload;
-    reinterpret_cast<ref_any&>(tmp) = std::move(value);
-    data.bits.payload = tmp;
+  nan_boxed(ref_any&& other) {
+    value_type value;
+    new (&value) ref_any(std::move(other));
+    data.bits.flag = true;
+    data.bits.nan = ieee754::quiet_nan;
+    data.bits.payload = value;
   }
   
-  ~derp() {
-    if(data.bits.nan && data.bits.flag) release();
-  }
-
+  
   
 };
 
@@ -89,12 +138,9 @@ public:
 
 
 int main(int, char**) {
+  nan_boxed x = make_ref<test>().any();
 
-  derp x;
-  x.set(1.0);
-
-  ref<test> y = make_ref<test>();
-  x.set(y.any());
-  x.set( make_ref<test>().any() );
+  // x = 1.0;
+  std::clog << "finished" << std::endl;
   return 0;
 }
