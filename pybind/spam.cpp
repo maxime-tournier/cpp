39537aaf4a4d539f2e11e64e83cd5312bd56e02f
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-12-15 19:22:07 +0100

    mew

--- pybind/spam.cpp
+++ pybind/spam.cpp
@@ -4,93 +4,144 @@
 #include <vector>
 
 #include <iostream>
+#include <boost/intrusive_ptr.hpp>
 
 template<class T>
-using ptr = std::shared_ptr<T>;
+using ptr = boost::intrusive_ptr<T>;
 
-// a base class for a c++ object that has a python counterpart: the two objects
-// will live exactly for the same duration, with python detecting and collecting
-// garbage.
-struct base {
+#define AS_GC(o) ((PyGC_Head *)(o)-1)
+ 
+static long& gc_refs(PyObject* self) {
+  PyGC_Head *gc = AS_GC(self);
+  return gc->gc.gc_refs;
+}
+
+
+struct py_base {
+  std::size_t rc = 0;
   PyObject* self = nullptr;
-  
-  base(PyObject* self) : self(self) {
-    // the c++ object keeps the python object alive, which in turn keeps the c++
-    // object alive. only the python gc can detect/break the cycle (see
-    // tp_traverse/tp_clear).
+
+  py_base(PyObject* self) : self(self) {
+    // c++ object keeps python object alive
     Py_XINCREF(self);
-    // precondition: self is a 'freshly' instantiated python object
-    std::clog << "base()" << std::endl;    
-  }
-  
-  virtual ~base() {
-    // the c++ object is expected to live as long as the python object, with
-    // python controlling the release. the only legal way this object can be
-    // destroyed is after tp_clear is called on the python object, which clears
-    // 'self' for this object. henceforth, 'self' must be null.
-    assert( !self && "python object outliving its c++ counterpart");
-    std::clog << "~base()" << std::endl;
+    std::clog << "py_base() " << this << " - " << self << std::endl;
   }
 
-  // python object
-  struct object {
-    PyObject_HEAD;
-    ptr<base> obj;
-  };
-
-  // python methods
-  static int tp_traverse(PyObject *self, visitproc visit, void *arg) {
-    // std::clog << "tp_traverse" << std::endl;
-                                  
-    // Py_VISIT notifies python gc about the c++/python/c++ cycle: to python,
-    // this object acts like a container containing itself. when python detects
-    // this object is unreachable, tp_clear will be called and the python object
-    // will be collected.
-
-    // note: we only allow python gc to collect this object when it becomes
-    // unreachable from the c++ side too
-    object* cast = reinterpret_cast<object*>(self);
-    if(cast->obj.unique()) {
-      Py_VISIT(self);
+  ~py_base() {
+    std::clog << "~py_base() " << this << " - " << self << std::endl;    
+    if(self) {
+      // python ref should have been cleared *before* destruction happens
+      // (during gc clear), or we may still have python ref pointing to the c++
+      // object
+      throw std::logic_error("attempting to destruct a c++ object with non-zero python ref");
     }
-    
-    return 0;
   }
 
-  static int tp_clear(PyObject *self) {
-    // std::clog << "tp_clear" << std::endl;
-    
-    object* cast = reinterpret_cast<object*>(self);
-    // the python object became unreachable: it will be collected. we notify the
-    // c++ object accordingly.
-    cast->obj->self = nullptr;
-
-    // c++ object must be released after this point, otherwise it will outlive
-    // its python counterpart.
-    assert( cast->obj.unique() && "c++ object outliving its python counterpart");
-    cast->obj.reset();
-    return 0;
-  }
-
-  // python type object
-  static PyTypeObject pto;
-
-
-  // convenience
   template<class Derived>
-  static Derived* as(PyObject* self) {
-    // TODO check python types
-    object* cast = reinterpret_cast<object*>(self);
-    return static_cast<Derived*>(cast->obj.get());
+  friend void intrusive_ptr_add_ref(Derived* self) {
+    ++self->rc;
+  }
+
+  template<class Derived>
+  friend void intrusive_ptr_release(Derived* self) {
+    std::clog << "decref: " << self << " " << self->rc << std::endl;
+    if(--self->rc == 0) {
+      delete self;
+      return;
+    }
+
   }
   
   
 };
 
-PyTypeObject base::pto = {
+
+
+// a c++ class extensible from python
+struct spam : py_base {
+  
+public:
+
+  using py_base::py_base;
+  
+  std::string bacon() {
+    PyObject* obj = PyObject_CallMethod(self, (char*)"bacon", NULL);
+
+    // TODO check obj is actually a string
+    std::string res = PyString_AsString(obj);
+    Py_XDECREF(obj);
+    
+    return res;
+  }
+
+  
+};
+
+static struct test { 
+
+  test() {
+    std::clog << "test" << std::endl;
+  }
+
+  ~test() {
+    std::clog << "~test" << std::endl;
+  }
+
+  
+  std::vector< ptr<spam> > spams;
+  
+} instance;
+
+// module methods
+static PyObject* some(PyObject* self, PyObject* args);
+static PyObject* clear(PyObject* self, PyObject* args);  
+
+struct py_spam {
+  PyObject_HEAD;
+  ptr<spam> obj;
+
+  static void dealloc(py_spam* self);
+  static int traverse(py_spam *self, visitproc visit, void *arg);
+  static int tp_clear(py_spam *self);
+
+
+  static PyObject* bacon(py_spam* self, PyObject* args, PyObject** kwargs) {
+    return PyString_FromString("standard bacon");
+  }
+
+  static PyObject* tp_new(PyTypeObject* cls, PyObject* args, PyObject* kwargs) {
+    std::clog << "tp_new" << std::endl;
+    PyObject* self = PyType_GenericAlloc(cls, 1);
+    
+    py_spam* py = reinterpret_cast<py_spam*>(self);
+
+    // allocate c++ object here
+    py->obj = new spam(self);
+    instance.spams.emplace_back(py->obj);
+    
+    return self;
+  }
+  
+};
+
+static PyMethodDef module_methods[] = {
+  {"some",  some, METH_VARARGS, "produce a limited quantity of spam"},
+  {"clear",  clear, METH_VARARGS, "clear all known spam instances"},    
+  {NULL}
+};
+
+static PyMethodDef spam_methods[] = {
+  {"bacon", (PyCFunction)py_spam::bacon, METH_KEYWORDS,
+   "gimme some bacon"
+  },
+  {NULL}  /* Sentinel */
+};
+
+
+static PyTypeObject spam_type = {
   PyVarObject_HEAD_INIT(NULL, 0)
-  "base",             /* tp_name */
-  sizeof(object), /* tp_basicsize */
+  "spam.spam",             /* tp_name */
+  sizeof(py_spam), /* tp_basicsize */
   0,                         /* tp_itemsize */
   0,                         /* tp_dealloc */
   0,                         /* tp_print */
@@ -108,124 +159,71 @@ PyTypeObject base::pto = {
   0,                         /* tp_setattro */
   0,                         /* tp_as_buffer */
   Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC | Py_TPFLAGS_BASETYPE,        /* tp_flags */
-  "base class for collectable objects",           /* tp_doc */
-  tp_traverse,
-  tp_clear,
-};
-
-
-
-// a user class 
-struct spam : base {
-  using base::base;
-
-  
-  std::string bacon() const {
-    PyObject* obj = PyObject_CallMethod(self, (char*) "bacon", NULL);
-    std::string res = PyString_AsString(obj);
-    Py_XDECREF(obj);
-    return res;
-  }
-  
-  static PyObject* tp_new(PyTypeObject* cls, PyObject* args, PyObject* kwargs) {
-    std::clog << "tp_new" << std::endl;
-    PyObject* self = PyType_GenericAlloc(cls, 1);
-
-    // allocate c++ object
-    base::object* cast = reinterpret_cast<base::object*>(self);
-    cast->obj = std::make_shared<spam>(self);
-
-    // keep a handle on c++ objects
-    static std::vector<ptr<base> > instances;
-  
-    return self;
-  }
-
-  static PyObject* py_bacon(PyObject* self, PyObject* args, PyObject* kwargs) {
-    return PyString_FromString("default bacon");
-  }
-  
-  static PyMethodDef tp_methods[];
-};
-
-PyMethodDef spam::tp_methods[] = {
-  {"bacon", (PyCFunction)py_bacon,  METH_KEYWORDS, "give the user some bacon"},
-  {NULL}
-};
-
-
-static const struct sentinel {
-  sentinel() { std::clog << "sentinel" << std::endl; }
-  ~sentinel() { std::clog << "~sentinel" << std::endl; }  
-} instance;
-
-
-
-
-static PyMethodDef module_methods[] = {
-  {NULL}
-};
-
-
-static PyTypeObject spam_type = {
-  PyVarObject_HEAD_INIT(NULL, 0)
-  "spam",             /* tp_name */
-  sizeof(base::object), /* tp_basicsize */
-  0,                         /* tp_itemsize */
-  0,                         /* tp_dealloc */
-  0,                         /* tp_print */
-  0,                         /* tp_getattr */
-  0,                         /* tp_setattr */
-  0,                         /* tp_compare */
-  0,                         /* tp_repr */
-  0,                         /* tp_as_number */
-  0,                         /* tp_as_sequence */
-  0,                         /* tp_as_mapping */
-  0,                         /* tp_hash */
-  0,                         /* tp_call */
-  0,                         /* tp_str */
-  0,                         /* tp_getattro */
-  0,                         /* tp_setattro */
-  0,                         /* tp_as_buffer */
-  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,        /* tp_flags */
   "spam bacon and spam",           /* tp_doc */
 };
 
 
+// implementation of module methods
+static PyObject* some(PyObject*, PyObject* args) {
 
-#define AS_GC(o) ((PyGC_Head *)(o)-1)
+  if(!PyArg_ParseTuple(args, "")) return NULL;
+  
+  PyObject* self = PyType_GenericNew(&spam_type, args, NULL);
+  py_spam* py = reinterpret_cast<py_spam*>(self);
 
-static long& gc_refs(PyObject* self) {
-  PyGC_Head *gc = AS_GC(self);
-  return gc->gc.gc_refs;
+  // gc cycle here
+  py->obj = new spam(self);
+
+  return self;
 }
 
-// static std::size_t force_gc(PyObject* self) {
-//   PyGC_Head *gc = AS_GC(self);
-//   _PyGCHead_SET_REFS(gc, _PyGC_REFS_TENTATIVELY_UNREACHABLE);  
-// }
+
+static PyObject* clear(PyObject*, PyObject* args) {
+  if(!PyArg_ParseTuple(args, "")) return NULL;
+  instance.spams.clear();
+  std::clog << "spam instances cleared" << std::endl;
+  Py_RETURN_NONE;
+}
 
 
+
+int py_spam::traverse(py_spam *self, visitproc visit, void *arg) {
+  std::clog << "tp_traverse: " << self << std::endl;
+
+  // 
+  if( self->obj->rc == 1 ) Py_VISIT(self);
+  else std::clog << "existing c++ refs outside python object, don't clear python object" << std::endl;
+  return 0;
+}
+
+int py_spam::tp_clear(py_spam *self) {
+  std::clog << "tp_clear: " << self << std::endl;
+
+  // object is in a cycle: il will get collected. we notify c++ about it, and
+  // prevent it from decref'ing during destruction, if any
+  std::clog << " c++ rc: " << self->obj->rc << std::endl;
+  self->obj->self = nullptr;
+  self->obj.reset();
+  
+  return 0;
+}
 
 
 PyMODINIT_FUNC initspam(void) {
-  if (PyType_Ready(&base::pto) < 0) return;
-  Py_INCREF(&base::pto);
   
-  // spam_type.tp_traverse = (traverseproc) py_spam::tp_traverse;
-  // spam_type.tp_clear = (inquiry) py_spam::tp_clear;  
-
-  spam_type.tp_new = spam::tp_new;
-  spam_type.tp_methods = spam::tp_methods;
-  spam_type.tp_base = &base::pto;  
+  // spam_type.tp_dealloc = (destructor) py_spam::dealloc;
+  spam_type.tp_traverse = (traverseproc) py_spam::traverse;
+  spam_type.tp_clear = (inquiry) py_spam::tp_clear;
+  spam_type.tp_methods = spam_methods;
+  spam_type.tp_new = py_spam::tp_new;
   
   // noddy_NoddyType.tp_new = PyType_GenericNew;
   if (PyType_Ready(&spam_type) < 0) return;
 
   PyObject* module = Py_InitModule3("spam", module_methods, 
                                     "Example module that creates an extension type.");
-  
-  Py_INCREF(&spam_type);
 
+  Py_INCREF(&spam_type);
+  
   PyModule_AddObject(module, "spam", (PyObject *)&spam_type);
 }
--- pybind/test.py
+++ pybind/test.py
@@ -1,36 +1,25 @@
-from __future__ import print_function
-
 import spam
 import gc
 
+
 def func():
-    s = spam.spam()
-    print('func', s.bacon())
+    s = spam.some()
+    print(s.bacon())
     
 func()
+    
+# spam.clear()
 
 
 class SuperSpam(spam.spam):
 
     def bacon(self): return 'super bacon'
 
+super_spam = SuperSpam()
+print(super_spam.bacon());
 
-def func2():
-    s = SuperSpam()
-    print('func2', s.bacon())
+# del super_spam
 
-
-func2()
-        
-# spam.clear()
-
-# x = spam.some()
-# del x
-# spam.clear()
-
-
-print('python end')
-# gc.set_debug(gc.DEBUG_LEAK)
 # print(gc.collect())
 # gc.collect()
 # gc.collect()
