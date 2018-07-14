7e6f705d4db9aae14a75979141ccc9bd591267d6
Maxime Tournier <tournier.maxime@gmail.com>
2018-07-14 22:39:25 +0200

    slip

---
+++ slip.cpp
@@ -0,0 +1,247 @@
+// -*- compile-command: "c++ -o slip2 slip.cpp" -*-
+
+// variant
+#include <type_traits>
+#include <utility>
+#include <new>
+
+// symbol
+#include <set>
+
+// refs
+#include <memory>
+
+
+#include <iostream>
+
+namespace {
+  template<std::size_t, class ... Args>
+  struct helper;
+
+  template<std::size_t I, class T>
+  struct helper<I, T> {
+    static constexpr std::size_t index(const T*) { return I; }
+
+    static void construct(void* dest, const T& source) { new (dest) T(source); }
+    static void construct(void* dest, T&& source) { new (dest) T{std::move(source)}; }
+  };
+
+
+  template<std::size_t I, class T, class ... Args>
+  struct helper<I, T, Args...> : helper<I + 1, Args... >, helper<I, T> {
+    using this_type = helper<I, T>;
+    using other_types = helper<I + 1, Args... >;
+
+    using this_type::index;
+    using other_types::index;
+
+    using this_type::construct;
+    using other_types::construct;
+  };
+  
+  struct destruct {
+    template<class T>
+    void operator()(const T& self) const { self.~T(); }
+  };
+
+  struct move {
+    template<class T>
+    void operator()(T& source, void* target) const {
+      new (target) T(std::move(source));
+    }
+  };
+
+  struct copy {
+    template<class T>
+    void operator()(T& source, void* target) const {
+      new (target) T(source);
+    }
+  };
+
+
+  template<class ... Func>
+  struct overload : Func... {
+    overload(const Func& ... func) : Func(func)... { }
+  };
+
+
+  
+  template<class Func, class ... Args>
+  struct visitor_result_type;
+
+  template<class Func, class ... Params, class T>
+  struct visitor_result_type<Func(Params...), T> {
+    using type = typename std::result_of<Func(T, Params...)>::type;
+  };
+
+  template<class Func, class T, class ... Args>
+  struct visitor_result_type<Func, T, Args...> : visitor_result_type<Func, T> {
+    
+    static_assert(std::is_same<typename visitor_result_type::type,
+                  typename visitor_result_type<Func, Args...>::type>::value,
+                  "all return types must be the same");
+  };
+  
+}
+  
+  
+template<class ... Args>
+class variant {
+  using storage_type = typename std::aligned_union<0, Args...>::type;
+  storage_type storage;
+
+  using index_type = unsigned char;
+  index_type index;
+
+  using helper_type = helper<0, Args...>;
+
+  template<class T, class Ret, class Self, class Func, class ... Params>
+  static Ret thunk(Self&& self, Func&& func, Params&& ... params) {
+    return std::forward<Func>(func)(std::forward<Self>(self).template get<T>(),
+                                    std::forward<Params>(params)...);
+  }
+
+  static void derived(const variant&);
+
+public:
+
+  template<class T>
+  const T& get() const { return reinterpret_cast<const T&>(storage); }
+
+  template<class T>
+  T& get() { return reinterpret_cast<T&>(storage); }
+
+  
+  template<class T, index_type index = helper_type::index( (T*) 0 )>
+  variant(T&& value) : index(index) {
+    helper_type::construct(&storage, std::forward<T>(value));
+  }
+
+  variant(variant&& other) : index(other.index) {
+    visit(other, move(), &storage);
+  }
+  
+  variant(const variant& other) : index(other.index) {
+    visit(other, copy(), &storage);
+  }
+
+  ~variant() { visit(*this, destruct()); }
+  
+  template<class Self, class Func, class ... Params>
+  friend auto visit(Self&& self, Func&& func, Params&& ... params) -> decltype(derived(self)) {
+    using ret_type = void;
+    using thunk_type = ret_type (*) (Self&&, Func&&, Params&&...);
+    
+    static const thunk_type table[] = {thunk<Args, ret_type, Self, Func, Params...>...};
+    
+    return table[self.index](std::forward<Self>(self),
+                             std::forward<Func>(func),
+                             std::forward<Params>(params)...);
+  }
+
+  template<class Self, class ... Func>
+  friend auto match(Self&& self, const Func& ... func) -> decltype(derived(self)) {
+    return visit(std::forward<Self>(self), overload<Func...>(func...));
+  }
+  
+};
+
+struct unit { };
+using boolean = bool;
+using integer = long;
+using real = double;
+
+class symbol {
+  const char* name;
+public:
+  
+  explicit symbol(const std::string& name) {
+    static std::set<std::string> table;
+    this->name = table.insert(name).first->c_str();
+  }
+
+  const char* get() const { return name; }
+  bool operator<(const symbol& other) const { return name < other.name; }
+  bool operator==(const symbol& other) const { return name == other.name; }  
+};
+
+template<class T>
+using ref = std::shared_ptr<T>;
+
+template<class T> struct cons;
+template<class T> using list = ref<cons<T>>;
+
+template<class T>
+struct cons {
+  T head;
+  list<T> tail;
+
+  cons(const T& head, const list<T>& tail) : head(head), tail(tail) { }
+  
+  friend list<T> operator>>=(const T& head, const list<T>& tail) {
+    return std::make_shared<cons>(head, tail);
+  }
+
+  template<class Func>
+  friend list< typename std::result_of<Func(T)>::type > map(const list<T>& self, const Func& func) {
+    if(!self) return {};
+    return func(self->head) >>= map(self->tail, func);
+  }
+  
+};
+
+
+struct sexpr : variant<real, integer, boolean, symbol, list<sexpr> > {
+  using sexpr::variant::variant;
+  using list = list<sexpr>;
+};
+
+
+struct lambda {
+  const list<symbol> args;
+  const sexpr body;
+};
+
+struct value : variant<real, integer, boolean, symbol, lambda, list<value> > {
+  using value::variant::variant;
+  using list = list<value>;
+};
+
+template<class Target>
+struct cast {
+
+  template<class T>
+  Target operator()(const T& self) const { return self; }
+  
+};
+
+
+// static value eval(const sexpr& expr) {
+//   return match(expr,
+//                [&](real x) -> value { return x; },
+//                [&](integer x) -> value { return x; },
+//                [&](boolean x) -> value { return x; },
+//                [&](lambda x) -> value { return x; },
+               
+//                [&](symbol x) -> value { return x; },
+//                [&](sexpr::list x) -> value {
+//                  return 1.0;
+//                });
+// }
+
+
+
+int main(int, char**) {
+
+  const sexpr e = 2.0 >>= symbol("michel") >>= sexpr::list();
+
+  match(e,
+        [](real d) { std::clog << "double" << std::endl; },
+        [](integer d)  { std::clog << "long" << std::endl; },
+        [](boolean d) { std::clog << "bool" << std::endl; },
+        [](symbol s)  { std::clog << "symbol" << std::endl; },
+        [](sexpr::list x){ std::clog << "list" << std::endl; }
+        );
+  
+  return 0;
+}
