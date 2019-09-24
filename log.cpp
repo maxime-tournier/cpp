47390360a0b52aa172cd7a8219dbe9de7243a7b6
Maxime Tournier <maxime.tournier@anatoscope.com>
2019-09-24 19:32:25 +0200

    log

--- log.cpp
+++ log.cpp
@@ -6,47 +6,80 @@
 #include <memory>
 #include <functional>
 
-enum level {
-            debug,
-            info,
-            warning,
-            error,
-            fatal,
-};
+namespace log {
+  enum level {
+              debug = 0,
+              info,
+              warning,
+              error,
+              fatal,
+              size
+  };
 
 
-using handler = std::function<void(const std::string& line, const char* emitter)>;
+  using handler = std::function<void(const std::string& line, const char* emitter)>;
 
-
-struct dispatch: std::stringbuf {
-  std::vector<handler> handlers;
-  
-  std::stringstream ss;
-  
-  virtual int sync() {
-    // start from leftovers
-    ss << str();
-
-    // split lines
-    std::string line;
-    while(std::getline(ss, line, '\n') && !ss.eof()) {
-      for(const auto& h: handlers) {
-        h(line, nullptr);
-      }
-    }
-    
-    // reset stringstream with leftovers, if any
-    ss = {};
-    ss << line;
-
-    // clear buffer
-    str("");
-    
-    return 0;
+  std::vector<handler>& handlers(log::level level) {
+    static std::vector<handler> table[log::level::size];
+    return table[level];
   }
-  
-};
 
+  class emitter {
+    std::string tag;
+  public:
+    friend std::ostream& operator<<(std::ostream& out, const emitter& self) {
+      // return out << "__" << tag << "@";
+    }
+
+    friend std::istream& operator>>(std::istream& in, emitter& self) {
+      char c;
+      if(in >> c && c == '[') {
+        while(in && in.get() != ']')
+      }
+
+      in.fail();
+      return in;
+    }
+
+  };
+
+
+  struct dispatch: std::stringbuf {
+    std::stringstream ss;
+
+    dispatch(log::level level, const char* emitter=nullptr):
+      level(level),
+      emitter(emitter) { }
+    
+    const log::level level;
+    const char* emitter = nullptr;
+    
+    virtual int sync() {
+      // start from leftovers
+      ss << str();
+
+      // split lines
+      std::string line;
+      while(std::getline(ss, line, '\n') && !ss.eof()) {
+        for(const auto& h: handlers(level)) {
+          h(line, emitter);
+        }
+      }
+    
+      // reset stringstream with leftovers, if any
+      ss = {};
+      ss << line;
+
+      // clear buffer
+      str("");
+    
+      return 0;
+    }
+  };
+
+  
+
+}
 
 void default_handler(const std::string& line, const char* emitter=nullptr) {
   std::cout << "process: " << line << '\n';
@@ -54,9 +87,9 @@ void default_handler(const std::string& line, const char* emitter=nullptr) {
 
 
 int main(int, char** ) {
-  dispatch buf;
-  buf.handlers.emplace_back(default_handler);
-
+  log::dispatch buf(log::info);
+  log::handlers(log::info).emplace_back(default_handler);
+  
   std::ostream s(&buf);
 
   s << "yo" << std::endl << "what" << std::flush << "dobidou" << "\n" << std::flush;
