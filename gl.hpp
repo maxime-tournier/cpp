1a58f448ad8a241f22ee5c20a0ce1ecb6fcad403
Maxime Tournier <tournier.maxime@gmail.com>
2020-06-13 16:32:20 +0200

    slowly learning modern opengl basics

--- gl.hpp
+++ gl.hpp
@@ -6,6 +6,8 @@
 #include <QApplication>
 #include <QOpenGLWidget>
 
+#include <iostream>
+
 namespace gl {
 
   struct buffer {
@@ -14,6 +16,7 @@ namespace gl {
     void bind() {
       if(!id) {
         glGenBuffers(1, &id);
+        assert(id);
       }
 
       glBindBuffer(GL_ARRAY_BUFFER, id);
@@ -21,31 +24,73 @@ namespace gl {
 
     template<class T>
     void data(const T* ptr, std::size_t size, GLenum mode) {
-      bind();
       glBufferData(GL_ARRAY_BUFFER, sizeof(T) * size, ptr, mode);
+    }
+
+
+    template<class Container>
+    void data(const Container& container, GLenum mode) {
+      data(container.data(), container.size(), mode);
     }
     
   };
 
+  template<class T>
+  struct traits;
+
+  template<std::size_t N>
+  struct traits<std::array<GLfloat, N>> {
+    static constexpr GLenum type = GL_FLOAT;
+    static constexpr GLuint size = N;
+    static_assert(N <= 4, "size error");
+  };
+  
+  static void error_check() {
+    GLenum err = glGetError();
+    if(err == GL_NO_ERROR) return;
+    std::cerr << "error: " << err << std::endl;
+    
+    error_check();
+  }
 
   struct vertex_attrib {
     GLuint index;
-    GLuint size;
-    GLenum type;
-    GLenum normalize = false;
-    GLuint stride = 0;
-    std::size_t offset = 0;
-    
-    void pointer() {
-      glVertexAttribPointer(size, size, type, normalize, stride, (void*) offset);
+
+    void pointer(GLuint size, GLenum type, GLenum normalize = GL_FALSE,
+                 GLuint stride = 0, std::size_t offset = 0) {
+      glVertexAttribPointer(index, size, type, normalize, stride, (void*) offset);
     }
 
-    auto lock() const {
+    auto enable() const {
       glEnableVertexAttribArray(index);
       return finally([index=index]{ glDisableVertexAttribArray(index); });
     };
+
+    // convenience using traits
+    template<class T>
+    void pointer(GLenum normalize = GL_FALSE) {
+      pointer(traits<T>::size, traits<T>::type, normalize);
+    }
+    
   };
 
+
+  // buffer + attrib combo
+  struct geometry {
+    gl::buffer buffer;
+    gl::vertex_attrib attrib;
+    
+    GLenum mode = GL_STATIC_DRAW;
+    GLenum normalize = GL_FALSE;
+    
+    template<class Container>
+    void data(const Container& container) {
+      buffer.bind();
+      buffer.data(container, mode);
+
+      attrib.pointer<typename Container::value_type>(normalize);
+    }
+  };
   
 }
 
--- mesh.cpp
+++ mesh.cpp
@@ -1,9 +1,12 @@
 #include "gl.hpp"
 
 #include <QApplication>
+
 #include <QOpenGLWidget>
+#include <QOpenGLShaderProgram>
 
 #include "eigen.hpp"
+#include <array>
 
 using namespace eigen;
 
@@ -11,25 +14,62 @@ struct mesh {
   vector<vec3> vertices;
   vector<vec3> normals;
   vector<vec2> texcoords;
-
 };
 
 
-struct Viewer: QOpenGLWidget {
-  void paintGL() override {
-    glBegin(GL_TRIANGLES);
-    glVertex3f(0, 0, 0);
-    glVertex3f(1, 0, 0);
-    glVertex3f(0, 1, 0);        
-    glEnd();
-  }
+// static void callback(GLenum source, GLenum type, GLuint id, GLenum severity,
+//                      GLsizei length, const GLchar* message,
+//                      const void* userParam) {
+//   std::fprintf(stderr,
+//                "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+//                (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type,
+//                severity, message);
+// }
 
+
+struct Viewer: QOpenGLWidget {
+  // gl::vertex_attrib position;
+  gl::geometry position, color;
+  QOpenGLShaderProgram program;
+  
   void initializeGL() override {
+    static std::vector<std::array<GLfloat, 3>> positions = {
+      {0, 0, 0},
+      {1, 0, 0},
+      {0, -1, 0},
+      {0, 1, 0},
+      {1, 0, 0},
+      {0, 0, 0},      
+    };
+
+
+    program.addShaderFromSourceCode(QOpenGLShader::Vertex,
+                                    "attribute highp vec4 vertex;\n"
+                                    // "uniform highp mat4 matrix;\n"
+                                    "void main(void)\n"
+                                    "{\n"
+                                    "   gl_Position = vertex;\n"
+                                    "}");
+    program.addShaderFromSourceCode(QOpenGLShader::Fragment,
+                                    // "uniform mediump vec4 color;\n"
+                                    "void main(void)\n"
+                                    "{\n"
+                                    "   gl_FragColor = vec4(1, 0, 0, 1);\n"
+                                    "}");
+
+    program.link();
+    program.bind();
     
+    position.attrib.index = program.attributeLocation("vertex");
+    position.data(positions);
   }
 
   void resizeGL(int w, int h) override {
+  }
 
+  void paintGL() override {
+    const auto lock = position.attrib.enable();
+    glDrawArrays(GL_TRIANGLES, 0, 6);    
   }
   
 };
