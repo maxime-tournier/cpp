a9d3aa4c3054020764d46e9ed57b399f36d3129e
Maxime Tournier <tournier.maxime@gmail.com>
2020-07-13 17:25:30 +0200

    ast machinery

--- ast.cpp
+++ ast.cpp
@@ -21,13 +21,45 @@ expr check(sexpr e) {
 }
 
 
+template<class Exception, class Cont>
+static auto rethrow_as(Cont cont) try {
+  return cont();
+} catch(std::exception& e) {
+  throw Exception(e.what());
+}
+
+
+struct parse_error: std::runtime_error {
+  using std::runtime_error::runtime_error;
+};
+
+
+template<class Cont>
+static void handle(Cont cont, std::ostream& err=std::cerr) try {
+  return cont();
+} catch(std::exception& e) {
+  err << "error: " << e.what() << std::endl;;
+};
+
+
 int main(int, char**) {
-  const auto parser = sexpr::parser();
+  const auto parser = sexpr::parser() >>= drop(parser::eos);
+  const auto parse = [&](const char* input) {
+    return rethrow_as<parse_error>([&]{
+      return run(parser, input);
+    });
+  };
+  
   
   repl([&](const char* input) {
-    const auto e = run(parser, input);
-    std::clog << e << std::endl;
+    return handle([&] {
+      const auto s = parse(input);
+      std::clog << s << std::endl;
+
+      const auto e = ast::check(s);
+    });
   });
+  
   return 0;
 }
 
