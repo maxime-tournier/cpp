c7e7c60d511f1ab2adbec69afeeacd77b5c74911
Maxime Tournier <maxime.tournier@anatoscope.com>
2020-09-10 16:31:26 +0200

    again

--- termux.cpp
+++ termux.cpp
@@ -4,6 +4,12 @@ int main() {
   std::cout << "cout" << std::endl;
   std::cerr << "cerr" << std::endl;
   std::clog << "clog" << std::endl;    
+
+  try {
+    throw std::runtime_error("error");
+  } catch(std::runtime_error& e) {
+    std::clog << e.what() << std::endl;;
+  }
   
   return 0;
 }
