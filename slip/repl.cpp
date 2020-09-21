9a41332800b935e7e733065543352f29563353f1
Maxime Tournier <tournier.maxime@gmail.com>
2020-09-21 22:18:41 +0200

    improve repl

---
+++ slip/repl.cpp
@@ -0,0 +1,35 @@
+#include "repl.hpp"
+
+#include <cstring>
+
+
+void repl(std::function<void(const char*)> handler,
+          const char* prompt,
+          const char* history) {
+  char* buf;
+
+  if(history) {
+    read_history(history);
+  }
+
+  const auto lock = finally([history] {
+    if(history) {
+      write_history(history);
+    }
+  });
+  
+  
+  while((buf = readline(prompt)) != nullptr) {
+    if(std::strlen(buf) > 0) {
+      add_history(buf);
+    }
+
+    std::string contents = buf;
+
+    // readline malloc's a new buffer every time.
+    std::free(buf);
+    
+    handler(contents.c_str());
+  }
+}
+
--- slip/repl.hpp
+++ slip/repl.hpp
@@ -5,35 +5,12 @@
 #include <readline/history.h>
 
 #include "finally.hpp"
-#include <cstring>
 
-template<class Cont>
-static void repl(Cont cont, const char* prompt = "> ",
-                 const char* history=nullptr) {
-  char* buf;
+#include <functional>
+#include <string>
 
-  if(history) {
-    read_history(history);
-  }
-
-  const auto lock = finally([history] {
-    if(history) {
-      write_history(history);
-    }
-  });
-  
-  
-  while((buf = readline(prompt)) != nullptr) {
-    if(std::strlen(buf) > 0) {
-      add_history(buf);
-    }
-    
-    cont(buf);
-    
-    // readline malloc's a new buffer every time.
-    std::free(buf);
-  }
-}
-
+void repl(std::function<void(const char*)> handler,
+          const char* prompt = "> ",
+          const char* history=nullptr);
 
 #endif
